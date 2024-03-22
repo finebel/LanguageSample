66c7863f813eead645e3529ecf031fabea0b997f
finebel <46824694+finebel@users.noreply.github.com>
2024-03-22 14:22:28 +0100

    Add Sample Project

---
+++ LanguageSample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  LanguageSample
+//
+//  Created by Finn Ebeling on 22.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = R.string.localizable.sample_test()
+    }
+
+}
+
